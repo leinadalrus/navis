95a73e00ce2dcaf943ba8cf275a3c0edcdcae348
David <daniel.david.surla@gmail.com>
2024-01-20 13:07:13 +1100

    updated pg.cc

--- src/pg.cc
+++ src/pg.cc
@@ -1,13 +1,19 @@
 #include <cstdio>
 #include <cstdlib>
-#include </usr/include/libpq-fe.h>
 
-int main(int argc, char *argv[])
+#ifdef _WIN32
+#include <C:\\dev\\vcpkg\\packages\\libpq_x86-windows\\include\\libpq-fe.h>
+#endif
+
+#ifdef __linux__
+#include </usr/include/libpq-fe.h>
+#endif
+
+int main(int argc, char* argv[])
 {
-  const char *conn_info;
-  PGconn *conn;
-  PGresult *res;
-  int fd, x, y;
+  const char* conn_info;
+  PGconn* conn;
+  PGresult* res;
 
   if (argc > 1)
     conn_info = argv[1];
@@ -27,4 +33,62 @@ int main(int argc, char *argv[])
 
     return 1;
   }
+
+  res = PQexec(
+          conn,
+          "explain select pg_catalog.set_config('search_path', '', false)"
+        );
+  if (PQresultStatus(res) != PGRES_TUPLES_OK)
+  {
+    PQclear(res);
+    PQfinish(conn);
+    return 1;
+  }
+  PQclear(res);
+
+  res = PQexec(conn, "begin");
+  if (PQresultStatus(res) != PGRES_COMMAND_OK)
+  {
+    PQclear(res);
+    PQfinish(conn);
+    return 1;
+  }
+  PQclear(res);
+
+  res = PQexec(
+          conn,
+          "declare myportal cursor for select * from pg_database"
+        );
+  if (PQresultStatus(res) != PGRES_COMMAND_OK)
+  {
+    PQclear(res);
+    PQfinish(conn);
+    return 1;
+  }
+  PQclear(res);
+
+  res = PQexec(
+          conn,
+          "fetch all in myportal"
+        );
+  if (PQresultStatus(res) != PGRES_TUPLES_OK)
+  {
+    PQclear(res);
+    PQfinish(conn);
+    return 1;
+  }
+  PQclear(res);
+
+  res = PQexec(
+          conn,
+          "close myportal"
+        );
+  PQclear(res);
+
+  res = PQexec(
+          conn,
+          "end"
+        );
+  PQclear(res);
+  PQfinish(conn);
 }
